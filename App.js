9e7c9487730f5332bda0526978afc305a753dd84
joChazaro <johana.chazarocortes@calbaptist.edu>
2021-03-08 15:59:00 -0800

    Fixed issues for nav errors, should run. See changes in App.js, and index.js

--- App.js
+++ App.js
@@ -22,18 +22,9 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet, Text, View } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
-
 import SignUp from './screens/SignUp.js'
-import new_page from './screens/new_page.js'
+//import new_page from './screens/new_page.js'
 
-
-// const App = StackNavigator () => {
-//    return (
-//       <SignUp />
-//    )
-// }
-// export default App
 const styles = StyleSheet.create({
    container: {
      flex: 1,
@@ -43,27 +34,10 @@ const styles = StyleSheet.create({
    },
  });
 
-const AppNavigator = createStackNavigator({
-   Signup: {
-      screen: SignUp
-   },
-   Home: {
-      screen: new_page
-   }
-});
-
-const AppContainer = createAppContainer(AppNavigator);
-
-export default class App extends React.Component {
-   render() {
-     return <NavigationContainer> {AppContainer}</NavigationContainer>;
-   }
- }
-
-//export default function App() {
-//   return (
-//     <NavigationContainer>{
-//       <SignUp />
-//    }</NavigationContainer>
-//   );
-// }
+export default function App() {
+  return (
+    <NavigationContainer>{
+      <SignUp />
+   }</NavigationContainer>
+  );
+}
